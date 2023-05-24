51d730061b002b52bb167e7760a07475ec74c8ab
n2ai <trinhh7@gator.uhd.edu>
2023-05-24 22:46:21 +0700

    header

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,20 @@
+export default function Header(){
+    return(
+        <div className="header-container">
+            <div className="header-container__services">
+                <p>Customer Service</p>
+                <p>Student Discount</p>
+                <p>Find a Store</p>
+            </div>
+            <div className="header-container__logo">
+                <img src="" className="" alt="logo"></img>
+            </div>
+            <div className="header-container__account">
+            
+                <span className="material-symbols-outlined">shopping_basket</span>
+                <span className="material-symbols-outlined"> person</span>
+                
+            </div>
+        </div>
+    )
+}
